4d9594fc42d03ca4abaf2da9ef9b5bab7ebc4bcd
whereand <2271653231@qq.com>
2018-01-16 11:28:53 +0800

    01

---
+++ src/main/java/utils/VerificationCode.java
@@ -0,0 +1,14 @@
+package utils;
+/**
+ * 生成验证码
+ * @author 薛宗艳
+ *
+ */
+public class VerificationCode {
+    //图片高度
+	private static final int IMG_HEIGHT = 100;
+	//图片宽度
+	private static final int IMG_WIGHT = 30;
+	//验证码长度
+	private static final int CODE_LENGTH = 4;
+}
